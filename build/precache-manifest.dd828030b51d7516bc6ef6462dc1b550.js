12f0ac72b297f789cc4e8173d6e0523cdf7e0c5c
Erick98 <ebernal.n98@gmail.com>
2022-05-15 17:52:14 -0500

    First commit
    Project upload

---
+++ build/precache-manifest.dd828030b51d7516bc6ef6462dc1b550.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "446fb8f523f1905b2359423b2cdd2f0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02c9d64aa4286c62565e",
+    "url": "/static/js/2.74438097.chunk.js"
+  },
+  {
+    "revision": "91d04624ce1b02bb4f4dfa7a1509a6ae",
+    "url": "/static/js/2.74438097.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e4e9b0c57bb6e491e86",
+    "url": "/static/js/main.a2acf029.chunk.js"
+  },
+  {
+    "revision": "6a59bd51eb184ae677f3",
+    "url": "/static/js/runtime-main.836b504c.js"
+  }
+]);
